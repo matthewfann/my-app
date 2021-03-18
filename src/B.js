057eedf628b58f786e158a66c866df493684a1b4
matthewfann <matthewenzo@icloud.com>
2021-03-18 13:06:08 +1300

    add test files

---
+++ src/B.js
@@ -0,0 +1,5 @@
+function B() {
+    return 1 + 1;
+}
+
+export default B;
---
+++ src/B.test.js
@@ -0,0 +1,5 @@
+import B from './B.js';
+test('returns 2', () => {
+    const result = B();
+    expect(result).toBe(2);
+  });
